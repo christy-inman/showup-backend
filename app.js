48441c470d5ba0ae35971306e089f0a6a0cccabe
github username <github email address>
2019-09-06 14:16:55 -0600

    get by id route working

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const cors = require('cors')
+const bodyParser = require('body-parser')
 const queries = require('./queries')
 
 app.listen(port)
+app.use(cors())
+app.use(bodyParser.json())
 
 app.get('/', (request, response) => {
     queries.allProtests()
         .then(protests => response.send(protests))
+}),
+app.get('/:id', (request, response) => {
+    queries.getById(request.params.id)
+        .then(protest => response.send(protest))
 })
 
 console.log(`Listening on port ${port}`)
--- queries.js
+++ queries.js
@@ -3,5 +3,10 @@ const database = require('./database-connection')
 module.exports = {
     allProtests() {
         return database('protests')
+    },
+    getById(id) {
+        return database('protests')
+            .where({id})
+            .first()
     }
 }
