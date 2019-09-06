6edec8ac226eb69c9d35aff47889b35908850f40
github username <github email address>
2019-09-06 13:43:25 -0600

    seeded

--- seeds/protests.js
+++ seeds/protests.js
@@ -1,13 +1,37 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('protests').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('protests').insert([
+        {
+          name: 'Mod 5 Walkout',
+          image: 'https://media.bizj.us/view/img/11133711/flatironschool3*750xx2560-1443-0-39.jpg',
+          date: '2019-09-09',
+          time: '3:30 PM',
+          location: '3601 Walnut Street Denver, CO 80205',
+          description: 'In protest of the New York office wanting to dampen the ambitions of the Denver campus students and staff, we will be walking out to show support and solidarity for the current curriculum standards pushed in Denver.',
+          donate: 'Venmo Link'
+        },
+        {
+          name: 'Reproduction Rights',
+          image: 'https://www.colorlines.com/sites/default/files/styles/embedded_image/public/images/articles/2015/01/54798165114c7a958458o.jpg?itok=kO0_UmCC&timestamp=1429386402',
+          date: '2019-09-21',
+          time: '4:00 PM - 7:00 PM',
+          location: '200 E Colfax Ave, Denver, CO 80203',
+          description: 'Current governments throughout the country are challenging Roe V. Wade ruling to keep abortions safe and legal for all women. Come out and join us to stand up against these bills. We\'ll gather in front of the capitol building, bring signs, coordinate outifts, and brainstorm chants.',
+          donate: 'https://www.weareplannedparenthoodaction.org/onlineactions/6iOI0_HnUUmPu_6_SRgayg2?sourceid=1006442&ms=4NALz2000K1N1A&gclid=Cj0KCQjwh8jrBRDQARIsAH7BsXcE5uVmd5DLPsqIEQGYkn_Rm7QXfJ7bQ_OuFZNE2GWmTd-leqNTqdEaAkwbEALw_wcB&gclsrc=aw.ds'
+        },
+        {
+          name: 'Amazon Rainforest Rehabilitation',
+          image: 'https://imageproxy.themaven.net/https%3A%2F%2Fimages.saymedia-content.com%2F.image%2FMTY2NDk4NjI4NjQ1NDMwNTE0%2Famazon-burning.jpg',
+          date: '2019-09-15',
+          time: '3:00 PM - 6:00 PM',
+          location: '200 E Colfax Ave, Denver, CO 80203',
+          description: 'As many of us know, the Amazon Rainforests have been on fire for weeks, and we are only half of the way through this years dry season, so it is likely they will get worse. Come out and show support for more strict logging and burning laws. We want to spread awareness about how people can help restore the areas that have burned, and prevent this level of distruction from happening again in the coming years. Show up, and spaek out with us.',
+          donate: 'https://support.worldwildlife.org/site/Advocacy?cmd=display&page=UserAction&id=1015&s_src=GoogleAdsAdvo&s_subsrc=amazon&gclid=Cj0KCQjwh8jrBRDQARIsAH7BsXdsb8Q-i_2xjJk_CW5M7IqQRUc2ryI0RpX5KaycaLd6RgYTulmmWvkaApVMEALw_wcB'
+        },
       ]);
     });
 };
