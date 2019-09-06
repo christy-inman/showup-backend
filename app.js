eaaadaf43407e8413a9e9a417c04c7cb5de3dcc6
github username <github email address>
2019-09-06 13:57:56 -0600

    get route working

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const queries = require('./queries')
 
 app.listen(port)
 
 app.get('/', (request, response) => {
-    response.send('CONNECTED')
+    queries.allProtests()
+        .then(protests => response.send(protests))
 })
 
 console.log(`Listening on port ${port}`)
