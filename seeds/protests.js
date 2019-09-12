5006457c0fe22952f99468db96b3c4529e40a1b1
github username <github email address>
2019-09-12 15:21:24 -0600

    seeds updated

--- seeds/protests.js
+++ seeds/protests.js
@@ -6,15 +6,6 @@ exports.seed = function(knex) {
       // Inserts seed entries
       return knex('protests').insert([
         {
-          title: 'Mod 5 Walkout',
-          image: 'https://media.bizj.us/view/img/11133711/flatironschool3*750xx2560-1443-0-39.jpg',
-          date: '2019-09-09',
-          time: '3:30 PM',
-          location: '3601 Walnut Street Denver, CO 80205',
-          description: 'In protest of the New York office wanting to dampen the ambitions of the Denver campus students and staff, we will be walking out to show support and solidarity for the current curriculum standards pushed in Denver.',
-          donate: 'Venmo Link'
-        },
-        {
           title: 'Reproduction Rights',
           image: 'https://www.colorlines.com/sites/default/files/styles/embedded_image/public/images/articles/2015/01/54798165114c7a958458o.jpg?itok=kO0_UmCC&timestamp=1429386402',
           date: '2019-09-21',
@@ -31,6 +22,15 @@ exports.seed = function(knex) {
           location: '200 E Colfax Ave, Denver, CO 80203',
           description: 'As many of us know, the Amazon Rainforests have been on fire for weeks, and we are only half of the way through this years dry season, so it is likely they will get worse. Come out and show support for more strict logging and burning laws. We want to spread awareness about how people can help restore the areas that have burned, and prevent this level of distruction from happening again in the coming years. Show up, and spaek out with us.',
           donate: 'https://support.worldwildlife.org/site/Advocacy?cmd=display&page=UserAction&id=1015&s_src=GoogleAdsAdvo&s_subsrc=amazon&gclid=Cj0KCQjwh8jrBRDQARIsAH7BsXdsb8Q-i_2xjJk_CW5M7IqQRUc2ryI0RpX5KaycaLd6RgYTulmmWvkaApVMEALw_wcB'
+        },
+        {
+          title: 'Capstone Projects',
+          image: 'https://media.bizj.us/view/img/11133711/flatironschool3*750xx2560-1443-0-39.jpg',
+          date: '2019-09-12',
+          time: '4:30 PM - 6:30 PM',
+          location: '3601 Walnut Street Denver, CO 80205',
+          description: 'As coaches we believe that three weeks is way too long for capstone projects. We will be around to chat about why we feel this way during the whole event',
+          donate: 'Venmo Link'
         },
       ]);
     });
